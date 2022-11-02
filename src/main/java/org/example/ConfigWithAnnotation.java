055119edac2effb88661a284a9389b8652c712fb
SolomonVP <Lum7023@yandex.ru>
2022-11-03 02:24:37 +0300

    @Value аннтоация для внедрения значений. В пропертях, либо напрямую в аннтоации.

--- src/main/java/org/example/ConfigWithAnnotation.java
+++ src/main/java/org/example/ConfigWithAnnotation.java
@@ -7,11 +7,13 @@ public class ConfigWithAnnotation {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 //        Cat myCat = context.getBean("cat", Cat.class);
-//
 //        myCat.say();
 
         Person person = context.getBean("personBean", Person.class);
         person.goHome();
+
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
 
         context.close();
     }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
@@ -9,6 +10,10 @@ public class Person {
 //    @Autowired
 //    @Qualifier("catBean")
     Pet pet;
+    @Value("${person.name}")
+    private String name;
+    @Value("${person.age}")
+    private int age;
 
     @Autowired
     public Person(@Qualifier("dogBean")Pet pet) {
@@ -32,5 +37,19 @@ public class Person {
         this.pet = pet;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
